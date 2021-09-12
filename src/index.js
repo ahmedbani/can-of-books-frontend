2e885d92bce0242724c4fc181f3b4d1fd7529945
ahmedbani <a13banisal@gmail.com>
2021-09-12 16:09:00 +0300

    fixing problems

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-yqh2xvci.us.auth0.com"
-    clientId="9QZod1tmo2z7H636R9lDmUznRaBvg0Aa"
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
     redirectUri={window.location.origin}
   >
     <App />
