fca5e25a771bbf2e353345c1587d5365358fb109
ahmedbani <a13banisal@gmail.com>
2021-09-12 15:51:18 +0300

    lab11

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import Header from "./Header";
+import { withAuth0 } from "@auth0/auth0-react";
+import IsLoadingAndError from "./IsLoadingAndError";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './Login';
+import BestBooks from './BestBooks';
+import Profile from "./components/profile";
 
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props);
-    return(
+    console.log("app", this.props);
+    let authenticated = this.props.auth0.isAuthenticated;
+    console.log(authenticated);
+    return (
       <>
         <Router>
           <IsLoadingAndError>
             <Header />
             <Switch>
               <Route exact path="/">
-                {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {
+                  /* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */
+                  authenticated ? <BestBooks/> : <Login /> 
+                }
               </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route exact path="/profile">
+                {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */
+                
+                authenticated ? <Profile /> : <Login /> 
+              }
+                
+              </Route>
             </Switch>
             <Footer />
           </IsLoadingAndError>
@@ -31,4 +41,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0 (App);
